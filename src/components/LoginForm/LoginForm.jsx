dd7e64ce56155902e42f461c1a85d617313065ed
Brandon Chau <brandon.chau578@gmail.com>
2024-06-25 11:13:46 -0700

    This is a change to the login page/ changed the styling

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -5,11 +5,18 @@ export const LoginForm = () => {
   return (
     <div className = "box">
         <div className = "LoginContainer">
-            <h1 className="LoginInfo"> Login </h1>
-            <input className="LoginInfo LoginUser"  type = "text" placeholder = "Username" />
-            <input className="LoginInfo LoginUser" type = "text" placeholder ="Password" />
-            <button className ="LoginInfo">Login</button>
+            <h1 className="LoginInfo"> Welcome to Ecotecture </h1>
+            <h2 className ="LoginHeader"> Email</h2>
+            <input className="LoginInfo LoginUser"  type = "text" placeholder = "Enter Email" />
+            <h2 className = "LoginHeader"> Password</h2>
+            <input className="LoginInfo LoginUser" type = "password" placeholder ="Enter Password" />
+            <div className = "LoginInfo SignedInContainer">
+              <input className = "SignedIn"  id="signedIn" type="checkbox"/>
+              <label for="signedIn"> Keep me signed in</label>
+            </div>
+            <button className ="LoginInfo">Sign In</button>
         </div>
+        
     </div>
   )
 }
