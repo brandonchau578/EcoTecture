e662bb357707639f00331aa105998168d29dc5ee
Brandon Chau <brandon.chau578@gmail.com>
2024-06-30 22:14:40 -0700

    There is an added change to the option bar for home types and highlighting when selected

--- src/components/OptionsBar/OptionsBar.jsx
+++ src/components/OptionsBar/OptionsBar.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './OptionsBar.css'
 import { UilSearch } from '@iconscout/react-unicons'
+import Select from 'react-select'
+
+const TypeOptions =[
+    {value: 'ForSale', label: 'For Sale'},
+    {value: 'ForRent', label: 'For Rent'},
+    {value: 'Sold', label: 'Sold'}
+]
+
+const HomeType=[
+    {value: 'Apartments', label: 'Apartments'},
+    {value:'Houses',label:'Houses'},
+    {value:'Townhomes', label:'Townhomes'}
+]
+
 
 function OptionsBar() {
+    const [selectedType, setSelectedType] = useState(null);
+    const [selectedHomeType, setSelectedHomeType] = useState(null);
+  
+    const handleTypeChange = (option) => {
+      setSelectedType(option);
+    };
+  
+    const handleHomeTypeChange = (option) => {
+      setSelectedHomeType(option);
+    };
   return (
     <div className = "OptionsBar">
-        <div className = "SearchBarContainer">
+        <div className = "OptionsBarContainer">
             <input className = "SearchBar" type="search" placeholder='City, Neighborhood, ZIP, Address' results/>
             <span className="SearchIcon"><UilSearch/></span>
+            <div className = "HouseOptions">
+                <Select 
+                options = {TypeOptions} 
+                className={`CustomSelect ${selectedType ? 'highlighted' : ''}`}
+                placeholder="Type"
+                classNamePrefix="react-select"
+                value={selectedType}
+                onChange={handleTypeChange}
+                />
+                <Select 
+                value={selectedHomeType}
+                onChange={handleHomeTypeChange}
+                options={HomeType}
+                className={`CustomSelect ${selectedHomeType ? 'highlighted' : ''}`}
+                placeholder="Home Type (4)"
+                classNamePrefix="react-select"
+                 />
+            </div>
         </div>
+        
     </div>
   )
 }
